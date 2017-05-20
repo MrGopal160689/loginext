08007ceed0c970fcdbd75a6a6787607cfa4b57d2
= <=>
2017-05-20 15:38:05 +0530

    server modified

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/', home);
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/dist'));
 
-function home(req, res) {
-    res.sendFile(__dirname + "/index.html");
-}
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
 
-var server = app.listen(3000, 'localhost', function() {
-
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log("Example app listening at http://%s:%s", host, port)
-
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
